27090228874053954f24cbdab834cf9921e314d1
zhelonkinaleksei <120109097+zhelonkinaleksei@users.noreply.github.com>
2023-02-16 12:30:52 +0300

    1

---
+++ js/popup.js
@@ -0,0 +1,28 @@
+const popupButton = document.querySelector(".support__button");
+const body = document.querySelector("body");
+
+
+if (popupButton){
+    const popup = document.querySelector('.popup');
+    popupButton.addEventListener("click", function (){
+        popup.classList.toggle("_open");
+        document.body.classList.toggle("_lock");
+     });
+  }
+
+const popupCancel = document.querySelector('.form__cancel');
+  
+if(popupCancel){
+    popupCancel.addEventListener("click", function(){
+        popup.classList.remove("_open");
+    });
+}
+
+popup.addEventListener("click", function(e){
+    if(!e.target.closest(".popup__form")) {
+        popup.classList.remove("_open");
+        document.body.classList.remove("_lock");
+    };
+})
+
+  
